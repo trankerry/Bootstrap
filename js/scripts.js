d5961f8a009aa09b0d5ff1cba9f8c49f15f591a5
Kerry <trankerry94@gmail.com>
2021-03-13 10:43:03 -0800

    added jQuery for opening modals

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,15 @@ $(function () {
         }
     });
 });
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    })
+})
+
+$(function () {
+    $("#loginButton").click(function () {
+        $("#loginModal").modal('show');
+    })
+})
