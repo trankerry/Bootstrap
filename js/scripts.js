85f9151bc15e57aa2862f6fbfb2fb41f2135a7e4
Kerry <trankerry94@gmail.com>
2021-03-16 22:52:08 -0700

    fixed sintax

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,9 @@ $(function () {
     $("#reserveButton").click(function () {
         $("#reserveModal").modal("show");
     })
-})
-
-$(function () {
     $("#loginButton").click(function () {
         $("#loginModal").modal('show');
     })
 })
+
+
